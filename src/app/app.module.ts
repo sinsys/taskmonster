5f1aef99990dab9247669274325b8bf28dd6e37b
Nicholas Hazel <nicholas.hazel@gmail.com>
2021-04-12 19:14:47 -0700

    feat: basic footer in place

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './scaffold/header/header.component';
 import { MainComponent } from './pages/main/main.component';
+import { FooterComponent } from './scaffold/footer/footer.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MainComponent
+    MainComponent,
+    FooterComponent,
+    DashboardComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
